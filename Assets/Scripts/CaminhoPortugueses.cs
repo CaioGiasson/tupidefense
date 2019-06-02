a8614a7d5f29618bd5723f17bd33c32e7286021f
Caio Giasson <cainitech@gmail.com>
2019-06-02 15:08:36 -0300

    caminho mais bonitinho, score, e outras correções menores

---
+++ Assets/Scripts/CaminhoPortugueses.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaminhoPortugueses : MonoBehaviour {
+
+	public static Transform[] pontos;
+
+	private void Awake() {
+		pontos = new Transform[transform.childCount];
+		for (int i = 0; i < transform.childCount; i++ ){
+			pontos[i] = transform.GetChild(i);
+		}
+	}
+
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public static ScoreManager manager;
+	public Text populacaoLabel;
+
+	public int populacaoAtual = 5000;
+
+	void Awake() {
+		manager = this;
+	}
+
+	public void RemoverPopulacao(int quantos){
+		populacaoAtual = Mathf.Max(0, populacaoAtual - quantos);
+		UpdateLabel();
+	}
+
+	void UpdateLabel(){
+		populacaoLabel.text = "População: " + populacaoAtual;
+	}
+
+}
